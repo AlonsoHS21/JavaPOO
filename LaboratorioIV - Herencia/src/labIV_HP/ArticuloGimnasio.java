92810d6d6eba4a957f9a1b967f0bdbb5ff7291f3
AlonsoHS21 <alonsohua20@gmail.com>
2021-03-14 21:57:41 -0300

    Video 5 contantes final
    Final es que el valor que se le asigne siempre sera fijo

--- LaboratorioIV - Herencia/src/labIV_HP/ArticuloGimnasio.java
+++ LaboratorioIV - Herencia/src/labIV_HP/ArticuloGimnasio.java
@@ -3,6 +3,9 @@ package labIV_HP;
 public class ArticuloGimnasio extends Articulo {
 		
 	private int peso;
+	private static final int gramos = 1000;
+	//static para que sea una variable de la clase y no de cada objeto creado
+	//final para que su valor no se pueda cambiar
 	
 	//Constructor
 	public ArticuloGimnasio(){
@@ -11,6 +14,10 @@ public class ArticuloGimnasio extends Articulo {
 	public ArticuloGimnasio(String nombre,int peso) {
 		super(nombre);
 		this.peso = peso;
+	}
+	
+	public int convierteKilosAGramos() {
+		return peso*gramos;
 	}
 
 	public float getPeso() {
--- LaboratorioIV - Herencia/src/labIV_HP/Principal.java
+++ LaboratorioIV - Herencia/src/labIV_HP/Principal.java
@@ -4,12 +4,21 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		/* Video 4 
 		ArticuloGimnasio artG = new ArticuloGimnasio();
 		artG.setNombre("Alonso");
 		artG.setPeso(100);
 		
 		System.out.println(artG.toString());
+		*/
+		
+		/* Video 5 */
+		ArticuloGimnasio artG = new ArticuloGimnasio();
+		artG.setNombre("Mancuerna");
+		artG.setPeso(100);
+		
+		System.out.println(artG.toString());
+		System.out.println(artG.convierteKilosAGramos());
 	}
 
 }
