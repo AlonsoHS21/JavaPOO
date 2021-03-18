f8caf43af81f37e88b566f2874315a4e61cceb71
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2021-03-18 15:47:24 -0300

    Video 14 - Errores

---
+++ LaboratorioIV - Errores/src/labIV_E/Principal.java
@@ -0,0 +1,28 @@
+package labIV_E;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/* ERRORES */
+		
+		//Existen 2 tipos de errores:
+		
+		// 1 - Errores en tiempo de compilacion (Sintaxis)
+		/*
+		String nombre;
+		nombre = 123;
+		*/
+		// 2 - Errores en tiempo de ejecucion (Excepciones)
+		
+		/*
+		int num1 = 9;
+		int num2 = 0;
+		int res = num1 / num2;
+		*/
+		//No me da error hasta que compile y ejecute
+		
+	}
+
+}
