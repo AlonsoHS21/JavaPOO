b7155b7eb7b685608a2208c3cc58064787a77f3b
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2021-03-18 09:12:09 -0300

    Creando clase Persona para Set

---
+++ Nueva carpeta/src/laboIV_S/Persona.java
@@ -0,0 +1,56 @@
+package laboIV_S;
+
+public class Persona {
+	
+	//Atributos
+	private String nombre;
+	private String apellido;
+	private int id;
+	private static int cont = 0;
+	
+	//Contructores
+	public Persona() {
+		cont++;
+		nombre = "Sin Nombre";
+		apellido = "Sin Apellido";
+		id = cont;
+	}
+	public Persona(String nombre,String apellido) {
+		cont++;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		id = cont;
+	}
+	
+	//Getters and Setters
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public int getID() {
+		return id;
+	}
+	public void setID(int id) {
+		this.id = id;
+	}
+	
+	//Metodo ToString();
+	@Override
+	public String toString() {
+		return "Nombre = " + nombre + ", Apellido = " + apellido + ", ID = " + id;
+	}
+	
+	
+	
+	
+	
+	
+}
--- Nueva carpeta/src/laboIV_S/Principal.java
+++ Nueva carpeta/src/laboIV_S/Principal.java
@@ -4,7 +4,20 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		/* SET */
+		//Maneja datos no repetidos y sin indices
+		//Set es una interfaz que tiene varias clases como:
+		//HashSet
+		//LinkedHashSet
+		//TreeSet
+		//EnumSet
+		
+		//Estos dos ejemplos son erroneos
+		//Set<Usuario> x = new Set()<Usuario>;
+		//Set<Usuario> x = new HashSet()<Usuario>;
+		
+		
+		
 	}
 
 }
